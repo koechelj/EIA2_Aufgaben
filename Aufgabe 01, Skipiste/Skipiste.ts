0096ca1b6db5e0a60c4648a6f4ad01f3a5edff8e
Lini <Lini@192.168.0.151>
2017-10-18 20:42:26 +0200

    Skipiste mit js Datei

--- Aufgabe 01, Skipiste/Skipiste.ts
+++ Aufgabe 01, Skipiste/Skipiste.ts
@@ -1,10 +1,12 @@
+namespace ersteAufgabe {
 window.addEventListener("load", init);
-
+    
+    
 function init():void{
-    let canvas:HTMLCanvasElement=document.getElementByTagName("canvas")[0];
+    let canvas:HTMLCanvasElement = document.getElementsByTagName("canvas")[0];
 console.log (canvas);
 //Zeichnung
-letcrc2:CanvasRenderingContext2D=canvas.getContext("2d");
+let crc2:CanvasRenderingContext2D=canvas.getContext("2d");
 console.log(crc2); 
    
 //Gondel Linie   
@@ -43,40 +45,40 @@ console.log(crc2);
     
 //Baum 1
  crc2.beginPath();
- crc2.moveto(150,380);
+ crc2.moveTo(150,380);
     crc2.lineTo(120,440);
     crc2.lineTo(180,440);
     crc2.stroke(); //schlieﬂe das Dreieck
-    crc2.fillstyle="green";
+    crc2.fillStyle="green";
     crc2.fill();
     
 //Baumstamm 1
-    crc2.fillstyle="maroon";
+    crc2.fillStyle="maroon";
     crc2.fillRect(130,440,10,30);
 
 //Baum 2
  crc2.beginPath();
- crc2.moveto(680,80);
+ crc2.moveTo(680,80);
     crc2.lineTo(650,140);
     crc2.lineTo(710,140);
     crc2.stroke(); //schlieﬂe das Dreieck
-    crc2.fillstyle="green";
+    crc2.fillStyle="green";
     crc2.fill();
     
 //Baumstamm 2
-    crc2.fillstyle="";
+    crc2.fillStyle="maroon";
     crc2.fillRect(660,140,10,30);
     
     //Berg 1
     crc2.beginPath();
-    crc2.moveto(400,50);
+    crc2.moveTo(400,50);
     crc2.lineTo(420,15);
     crc2.lineTo(440,50);
     crc2.stroke(); //Ende des Pfads
    
      //Berg 2
     crc2.beginPath();
-    crc2.moveto(440,50);
+    crc2.moveTo(440,50);
     crc2.lineTo(460,15);
     crc2.lineTo(480,50);
     crc2.stroke(); //Ende des Pfads
@@ -84,10 +86,10 @@ console.log(crc2);
     
      //Berg 3
     crc2.beginPath();
-    crc2.moveto(470,35);
+    crc2.moveTo(470,35);
     crc2.lineTo(500,15);
     crc2.lineTo(520,50);
     crc2.stroke(); //Ende des Pfads
     
-    
+    }
     }
