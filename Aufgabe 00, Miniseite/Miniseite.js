e3f4df1bfeae32b4772ee91802f3c55d41c308b4
Lini <Lini@192.168.0.151>
2017-10-26 18:39:50 +0200

    ...

--- Aufgabe 00, Miniseite/Miniseite.js
+++ Aufgabe 00, Miniseite/Miniseite.js
@@ -2,7 +2,7 @@ function Eingabefeld() {
     let Eingabe = prompt("Geben Sie bitte Ihren Namen ein:", "");
     //wenn Eingabe ungleich null bzw. etwas eingegeben wurde, dann folgendes ausgeben 
     if (Eingabe != null) {
-        document.getElementById("Hallo").innerHTML = "Hallo" + Eingabe + ", sch�n von dir zu h�ren!";
+        document.getElementById("Hallo").innerHTML = "Hallo " + Eingabe + ", schoen von dir zu hoeren!";
     }
 }
 //# sourceMappingURL=Miniseite.js.map
--- Aufgabe 00, Miniseite/Miniseite.ts
+++ Aufgabe 00, Miniseite/Miniseite.ts
@@ -2,6 +2,6 @@ function Eingabefeld(): void {
     let Eingabe:string = prompt("Geben Sie bitte Ihren Namen ein:","");
     //wenn Eingabe ungleich null bzw. etwas eingegeben wurde, dann folgendes ausgeben 
     if (Eingabe != null){
-    document.getElementById("Hallo").innerHTML="Hallo"+ Eingabe +", schön von dir zu hören!";
+    document.getElementById("Hallo").innerHTML="Hallo "+ Eingabe +", schoen von dir zu hoeren!";
     }
 }
