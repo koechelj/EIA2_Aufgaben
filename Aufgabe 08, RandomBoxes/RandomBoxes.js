54a58b0c4327aedde4f2d37a60af56ba89b31a06
Lini <Lini@192.168.0.150>
2017-12-07 22:31:29 +0100

    ...

--- Aufgabe 08, RandomBoxes/RandomBoxes.js
+++ Aufgabe 08, RandomBoxes/RandomBoxes.js
@@ -1,19 +1,30 @@
 var RandomBoxes;
 (function (RandomBoxes) {
     window.addEventListener("load", init);
-    window.addEventListener("keydown", keypress);
-    function keypress(_event) {
-        console.log(_event.keyCode);
-    }
     function init() {
-        for (let i = 0; i < 100; i++) {
-            let h = document.createElement("h2");
-            //console.log(h);
-            h.innerText = "Hallo, ich bin h2-Element Nr." + i;
-            document.body.appendChild(h);
-            if (i == 3)
-                h.style.color = "red";
+        //prompt Fenster �ffnen, welches nachfragt
+        var eingegebeneZahl = prompt("Wie viele Quadrate m�chten Sie sehen? Auf einer Skala von 10 bis 100.");
+        //var quadratAnzahl vom Typ eingegebeneZahl
+        var quadratAnzahl = parseInt(eingegebeneZahl); //die eingegebeneZahl soll vom string in eine number umgewandelt werden
+        //wenn die eingegebeneZahl gr��er bzw. = 10 ist und wenn sie kleiner bzw. = 100 ist, dann...
+        if (quadratAnzahl >= 10 && quadratAnzahl <= 100) {
+            //...for Schleife ausf�hren, welche die entsprechende Quadratanzahl generiert
+            for (var i = 0; i < quadratAnzahl; i++) {
+                zeichneQuadrat(Math.random() * 400, Math.random() * 200, "hsl(" + Math.random() * 360 + ", 100%, 50%)", 50, 50);
+            }
         }
+        else {
+            alert("Da ist wohl etwas schief gelaufen. Versuchen Sie es bitte erneut.");
+        }
+    }
+    function zeichneQuadrat(X, Y, Farbe, Hoehe, Breite) {
+        var div = document.createElement("div"); //div Element erstellen
+        div.style.width = Breite.toString(); //muss f�r .style in string umgewandelt werden
+        div.style.height = Hoehe.toString();
+        div.style.backgroundColor = Farbe;
+        div.style.display = "inline"; //inline, weil die die Quadrate nicht alle in einer neuen Zeile beginnen sollen (div ist normalerweise Block Element)
+        div.style.position = "absolute"; //div Elemente sind mit absolute losgel�st vom Textfluss
+        document.body.appendChild(div); //div in body einf�gen
     }
 })(RandomBoxes || (RandomBoxes = {}));
 //# sourceMappingURL=RandomBoxes.js.map
--- Aufgabe 08, RandomBoxes/RandomBoxes.ts
+++ Aufgabe 08, RandomBoxes/RandomBoxes.ts
@@ -1,19 +1,37 @@
 namespace RandomBoxes {
     window.addEventListener("load", init);
-    window.addEventListener("keydown", keypress);
 
-    function keypress(_event: KeyboardEvent): void {
-        console.log(_event.keyCode);
-    }
 
     function init(): void {
-        for (let i: number = 0; i < 100; i++) {
-            let h: HTMLHeadingElement = document.createElement("h2");
-            //console.log(h);
-            h.innerText = "Hallo, ich bin h2-Element Nr." + i;
-            document.body.appendChild(h);
-            if (i == 3) 
-                h.style.color = "red";
+
+        //prompt Fenster öffnen, welches nachfragt
+        var eingegebeneZahl: string = prompt("Wie viele Quadrate möchten Sie sehen? Auf einer Skala von 10 bis 100.");
+        //var quadratAnzahl vom Typ eingegebeneZahl
+        var quadratAnzahl: number = parseInt(eingegebeneZahl); //die eingegebeneZahl soll vom string in eine number umgewandelt werden
+
+        //wenn die eingegebeneZahl größer bzw. = 10 ist und wenn sie kleiner bzw. = 100 ist, dann...
+        if (quadratAnzahl >= 10 && quadratAnzahl <= 100) {
+
+            //...for Schleife ausführen, welche die entsprechende Quadratanzahl generiert
+            for (var i: number = 0; i < quadratAnzahl; i++) {
+                zeichneQuadrat(Math.random() * 400, Math.random() * 200, "hsl(" + Math.random() * 360 + ", 100%, 50%)", 50, 50);
+                //zufällige X und Y Position, Farbe, feste Höhe, feste Breite
+            }
+            //andernfalls soll eine Fehlermeldung erscheinen
+        } else {
+            alert("Da ist wohl etwas schief gelaufen. Versuchen Sie es bitte erneut.");
         }
+    }
+
+
+    function zeichneQuadrat(X: number, Y: number, Farbe: string, Hoehe: number, Breite: number): void {
+
+        var div: HTMLDivElement = document.createElement("div"); //div Element erstellen
+        div.style.width = Breite.toString(); //muss für .style in string umgewandelt werden
+        div.style.height = Hoehe.toString();
+        div.style.backgroundColor = Farbe; 
+        div.style.display = "inline"; //inline, weil die die Quadrate nicht alle in einer neuen Zeile beginnen sollen (div ist normalerweise Block Element)
+        div.style.position = "absolute"; //div Elemente sind mit absolute losgelöst vom Textfluss
+        document.body.appendChild(div); //div in body einfügen
     }
 }
