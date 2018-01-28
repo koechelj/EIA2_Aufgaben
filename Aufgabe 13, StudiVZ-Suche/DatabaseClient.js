b9a90a8e3f81fb1de764c79333dfebab959fa2a5
Lini <Lini@192.168.0.150>
2018-01-28 01:05:23 +0100

    ...

---
+++ Aufgabe 13, StudiVZ-Suche/DatabaseClient.js
@@ -0,0 +1,48 @@
+var Aufgabe13;
+(function (Aufgabe13) {
+    window.addEventListener("load", init);
+    function init(_event) {
+        console.log("Init");
+        //insert f�gt ein Dokument in die Collection ein
+        let insertButton = document.getElementById("insert"); //insert f�gt ein Dokument in die Collection ein
+        let refreshButton = document.getElementById("refresh");
+        insertButton.addEventListener("click", insert);
+        refreshButton.addEventListener("click", refresh);
+    }
+    function insert(_event) {
+        let inputs = document.getElementsByTagName("input");
+        let query = "command=insert";
+        query += "&name=" + inputs[0].value;
+        query += "&firstname=" + inputs[1].value;
+        query += "&matrikel=" + inputs[2].value;
+        console.log(query);
+        sendRequest(query, handleInsertResponse);
+    }
+    function refresh(_event) {
+        let query = "command=find"; //find findet alle Daten, die mit dem AbfrageQuery �bereinstimmen
+        sendRequest(query, handleFindResponse);
+    }
+    function sendRequest(_query, _callback) {
+        let xhr = new XMLHttpRequest();
+        //xhr.open("GET", "http://localhost:8100?" + _query, true);
+        xhr.open("GET", "https://eia2-servertest-koechelj.herokuapp.com/?" + _query, true);
+        xhr.addEventListener("readystatechange", _callback);
+        xhr.send();
+    }
+    function handleInsertResponse(_event) {
+        let xhr = _event.target;
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            alert(xhr.response);
+        }
+    }
+    function handleFindResponse(_event) {
+        let xhr = _event.target;
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            let output = document.getElementsByTagName("textarea")[0];
+            output.value = xhr.response;
+            let responseAsJson = JSON.parse(xhr.response);
+            console.log(responseAsJson);
+        }
+    }
+})(Aufgabe13 || (Aufgabe13 = {}));
+//# sourceMappingURL=DatabaseClient.js.map
